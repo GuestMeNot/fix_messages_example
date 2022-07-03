784d2621b571434ac6f8f6b75635fd4869234291
GuestMeNot <nadine_m_mckenzie@yahoo.com>
2022-07-03 13:39:51 -0400

    better naming

--- src/coinbase.rs
+++ src/coinbase.rs
@@ -1,7 +1,7 @@
-pub mod fix_decoder;
+pub mod message_encoder_decoder;
 
 /// **NOTE**: `coinbase_fix_types` module is generated!
 pub mod coinbase_fix_types;
 
 pub use coinbase_fix_types::*;
-pub use fix_decoder::*;
+pub use message_encoder_decoder::*;
